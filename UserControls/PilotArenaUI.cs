c41017e30cbf547756e965b4107a3e96741bbacc
yangnyeom06 <lyujin1204@gmail.com>
2026-05-16 05:14:33 +0900

    PilotArenaUI 작업 완료

--- UserControls/PilotArenaUI.cs
+++ UserControls/PilotArenaUI.cs
@@ -1,10 +1,18 @@
-﻿namespace DonkeyUI.UserControls
+﻿namespace DataManager.UserControls
 {
     public partial class PilotArenaUI : UserControl
     {
         public PilotArenaUI()
         {
             InitializeComponent();
+        }
+
+        private void btnModelAdd_Click(object sender, EventArgs e)
+        {
+            if (flpModule.Controls.Count >= 2) return;
+
+            var module = new ModelTestModule();
+            flpModule.Controls.Add(module);
         }
     }
 }
