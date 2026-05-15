ce66e762ae65f94d17a206af771c0ad1fa57c23e
yangnyeom06 <lyujin1204@gmail.com>
2026-05-16 03:51:31 +0900

    수정

--- Services/Trainer/Temp1.cs
+++ Services/Trainer/Temp1.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DataManager.Services.PilotArena
 {
-    internal class Temp
+    internal class Temp1
     {
         // PilotArena를 Github에 올리기 위한 임시 스크립트
         // 작업할때는 이 스크립트를 삭제하고 작업
